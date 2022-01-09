3a761e6ba84c77ab3adf64f598dc0ba3e593d041
GYUTAEKIM <kkt219a@gmail.com>
2022-01-09 14:39:50 +0900

    feat: 초기 프로젝트 환경 설정

---
+++ beginningcarrot/src/main/java/numble/beginningcarrot/BeginningcarrotApplication.java
@@ -0,0 +1,13 @@
+package numble.beginningcarrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginningcarrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginningcarrotApplication.class, args);
+	}
+
+}
