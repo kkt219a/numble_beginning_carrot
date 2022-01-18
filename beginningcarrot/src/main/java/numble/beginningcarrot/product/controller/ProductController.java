6367571560bd9bcd547a2756b3dbfad9cdcba901
GYUTAEKIM <kkt219a@gmail.com>
2022-01-18 23:24:13 +0900

    feat: 상품의 컨트롤러 및 기능 정의
    - 상품의 컨트롤러를 생성 후 필요한 기능들을 나열한다

---
+++ beginningcarrot/src/main/java/numble/beginningcarrot/product/controller/ProductController.java
@@ -0,0 +1,61 @@
+package numble.beginningcarrot.product.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/product")
+public class ProductController {
+
+	/**
+	 * Product를 페이지 별로 DTO로 반환
+	 */
+	@GetMapping
+	public ResponseEntity<Void> findAllProduct() {
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 특정 Product 상세보기
+	 * @param productId product의 Id
+	 * @return 특정 Product의 DTO
+	 */
+	@GetMapping("/{productId}")
+	public ResponseEntity<Void> findProduct(@PathVariable Long productId) {
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 새 Product 생성
+	 */
+	@PostMapping
+	public ResponseEntity<Void> createProduct() {
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 특정 Product 수정
+	 * @param productId product의 Id
+	 */
+	@PatchMapping("/{productId}")
+	public ResponseEntity<Void> updateProduct(@PathVariable Long productId) {
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 특정 Product 삭제
+	 * @param productId product의 Id
+	 */
+	@PatchMapping("/{productId}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+		return ResponseEntity.ok().build();
+	}
+}
